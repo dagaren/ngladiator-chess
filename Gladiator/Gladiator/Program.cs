dc7584af2f5291e137a1b5d5728acf6d084b276e
David Garcinuño Enríquez <dagaren@gmail.com>
2015-06-09 23:28:35 +0200

    Created solution, main project and test project
    Related Work Items: #25

---
+++ Gladiator/Gladiator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gladiator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Normal termination of the program");
+        }
+    }
+}
