e78073ad9295d74cafeee3c9fc2662737f3ae9be
David Garcinuño Enríquez <dagaren@gmail.com>
2015-06-10 23:42:27 +0200

    Revert "Implemented very basic command reading logic"
    This reverts commit 107ed4127503ba3a60b8c61650589e8f29486bf5.

--- Gladiator/Gladiator/Program.cs
+++ Gladiator/Gladiator/Program.cs
@@ -6,23 +6,6 @@ namespace Gladiator
     {
         static void Main(string[] args)
         {
-            bool exit = false;
-
-            while(!exit)
-            {
-                string command = Console.ReadLine();
-
-                switch(command)
-                {
-                    case "xboard":
-                        Console.WriteLine("> xboard command received!");
-                        break;
-                    case "quit":
-                        exit = true;
-                        break;
-                }
-            }
-
             Console.WriteLine("Normal termination of the program");
         }
     }
