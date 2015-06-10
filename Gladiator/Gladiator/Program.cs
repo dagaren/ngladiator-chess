a20567b41d2c7141a5bc9e28f290d0e76810ecea
David Garcinuño Enríquez <dagaren@gmail.com>
2015-06-10 23:43:30 +0200

    Implemented very basic command reader logic
    Related Work Items: #26

--- Gladiator/Gladiator/Program.cs
+++ Gladiator/Gladiator/Program.cs
@@ -6,6 +6,23 @@ namespace Gladiator
     {
         static void Main(string[] args)
         {
+            bool exit = false;
+
+            while (!exit)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "xboard":
+                        Console.WriteLine("> xboard command received!");
+                        break;
+                    case "quit":
+                        exit = true;
+                        break;
+                }
+            }
+
             Console.WriteLine("Normal termination of the program");
         }
     }
