c8ee20fc4eb717d2ade18a48d8d5c2b087578f92
David Garcinuño Enríquez <dagaren@gmail.com>
2015-06-30 00:22:57 +0200

    Added algebraic coordinate notation parser
    Related Work Items: #41

---
+++ Gladiator/Gladiator/Representation/Move.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator.Representation
+{
+    class Move
+    {
+        public Square Source { get; set; }
+        public Square Destination { get; set; }
+
+        public Piece Promotion { get; set; }
+    }
+}
