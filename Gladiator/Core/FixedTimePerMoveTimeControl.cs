4ef2fad08d36de1dd18a242d4248e6e0a1defdc5
David Garcinuño Enríquez <dagaren@gmail.com>
2015-09-19 18:14:04 +0200

    Added missing files

---
+++ Gladiator/Core/FixedTimePerMoveTimeControl.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator.Core
+{
+    public class FixedTimePerMoveTimeControl : ITimeControl
+    {
+        private TimeSpan timePerMove;
+
+        public FixedTimePerMoveTimeControl(TimeSpan timePerMove)
+        {
+            this.timePerMove = timePerMove;
+        }
+
+        public TimeSpan GetIncrement(int numMove)
+        {
+            return TimeSpan.FromSeconds(0);
+        }
+
+        public int GetMovesToNextControl(int numMove)
+        {
+            return 1;
+        }
+
+        public TimeSpan PreMoveTime(int numMove, TimeSpan previousTime)
+        {
+            return this.timePerMove;
+        }
+
+        public TimeSpan PostMoveTime(int numMove, TimeSpan previousTime)
+        {
+            return previousTime;
+        }
+    }
+}
---
+++ Gladiator/Core/TimeControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gladiator.Core
+{
+    public class TimeControl : ITimeControl
+    {
+        public int NumMoves { get { return this.numMoves; }  }
+
+        public TimeSpan BaseTime { get { return this.BaseTime; } }
+
+        public TimeSpan Increment { get { return this.increment; } }
+
+        private int numMoves;
+
+        private TimeSpan baseTime;
+
+        private TimeSpan increment;
+
+        public TimeControl(int numMoves, TimeSpan baseTime, TimeSpan increment)
+        {
+            this.numMoves = numMoves;
+            this.baseTime = baseTime;
+            this.increment = increment;
+        }
+
+        public TimeSpan GetIncrement(int numMove)
+        {
+            return this.increment;
+        }
+        public int GetMovesToNextControl(int numMove)
+        {
+            return this.numMoves == 0 ? -1 : this.numMoves - ((numMove - 1) % this.numMoves);
+        }
+
+        public TimeSpan PreMoveTime(int numMove, TimeSpan previousTime)
+        {
+            if(numMove == 1)
+            {
+                return this.baseTime;
+            }
+            else if(this.numMoves > 0 && numMove % this.numMoves == 1)
+            {
+                return this.baseTime.Add(previousTime);
+            }
+
+            return previousTime;
+        }
+
+        public TimeSpan PostMoveTime(int numMove, TimeSpan previousTime)
+        {
+            return previousTime.Add(this.increment);
+        }
+    }
+}
