6b2136c7137bb4cf093c74d2b8291d337a3cdb47
David Garcinuño Enríquez <dagaren@gmail.com>
2017-11-14 00:02:30 +0100

    Created solution and main project

---
+++ Gladiator/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Gladiator
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("NORMAL TERMINATION OF THE PROGRAM");
+        }
+    }
+}
